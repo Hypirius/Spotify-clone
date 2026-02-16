49bc5463a84a58696b42abc414a077159762c501
Hypirius <zuhayerhossain25@gmail.com>
2026-02-16 09:52:17 +0600

    feat: add Search ability style: centered document
    chore: add TailwindCSS for style management

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,8 +1,15 @@
 import { useState } from "react"
 import "./App.css"
+import SongForm from "./SongForm"
 
 function App() {
-  return <></>
+  const [search, setSearch] = useState<string>("")
+  return (
+    <main className="w-full h-screen flex items-center justify-center">
+      <SongForm query={search} setQuery={setSearch} />
+      <section id="searched-songs-container"></section>
+    </main>
+  )
 }
 
 export default App
