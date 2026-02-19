b4153a99ca71f3789437a7b918218953e1112637
Hypirius <zuhayerhossain25@gmail.com>
2026-02-19 19:30:28 +0600

    feat: Add AudioPlayer with playback controls(using test audio)

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,12 +1,42 @@
-import { useState } from "react"
-import "./App.css"
+import { useState, useEffect } from "react"
 import SongForm from "./SongForm"
+import AudioPlayer from "./AudioPlayer"
 
 function App() {
   const [search, setSearch] = useState<string>("")
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isError, setIsError] = useState<string | null>(null)
+
+  useEffect(() => {
+    setIsLoading(true)
+    setIsError(null)
+
+    const controller = new AbortController()
+    async function fetchSongResult() {
+      try {
+        const response = await fetch("/test.json", {
+          signal: controller.signal,
+        })
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch data")
+        }
+      } catch (error) {
+        setIsError(error.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchSongResult()
+
+    return () => controller.abort()
+  }, [search])
+
   return (
-    <main className="w-full h-screen flex items-center justify-center">
+    <main className="w-full h-screen flex flex-col items-center justify-center">
       <SongForm query={search} setQuery={setSearch} />
+      <AudioPlayer />
       <section id="searched-songs-container"></section>
     </main>
   )
